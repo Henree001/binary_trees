a6b84332a533a028268f02322402367f26158dcf
Henree001 <okaforhenry779@gmail.com>
2023-03-10 18:21:43 +0100

    tasks

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,17 +7,19 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int b_factor_right, b_factor_left;
+	int b_factor_right = 0, b_factor_left = 0;
 
 	if (tree == NULL)
 		return (0);
 	if (tree->left)
+	{
 		b_factor_left = binary_tree_height(tree->left);
-	else
-		b_factor_left = 1;
+		b_factor_left += 1;
+	}
 	if (tree->right)
+	{
 		b_factor_right = binary_tree_height(tree->right);
-	else
-		b_factor_right = 1;
+		b_factor_right += 1;
+	}
 	return (b_factor_left - b_factor_right);
 }
